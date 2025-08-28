0e57f32485650c623932c40b9f83931dc4a2fee0
Juan Jose Denk Guido <juanjosedenk05@gmail.com>
2025-08-28 17:06:11 -0300

    Ejercicio 3, inciso a

--- tp2/ej3/Surtidor.java
+++ tp2/ej3/Surtidor.java
@@ -1,16 +1,61 @@
 public class Surtidor{
     // Atributos de clase
-    private static int maximaCarga = 500;
+    private static final int maximaCarga = 20000;
     // Atributos de instancia
     private int cantGasoil;
     private int cantSuper;
     private int cantPremium;
     // Constructor
     public Surtidor(){
-        cantGasoil = 20000;
-        cantSuper = 20000;
-        cantPremium = 20000;
+        cantGasoil = maximaCarga;
+        cantSuper = maximaCarga;
+        cantPremium = maximaCarga;
     }
     // Comandos
-    
+    public void llenarDepositoGasoil(){
+        cantGasoil = maximaCarga;
+    }
+    public void llenarDepositoSuper(){
+        cantSuper = maximaCarga;
+    }
+    public void llenarDepositoPremium(){
+        cantPremium = maximaCarga;
+    }
+    // Requiere litros > 0
+    public void extraerGasoil(int litros){
+        if (cantGasoil > litros)
+            cantGasoil -= litros;
+        else
+            cantGasoil = 0;
+    }
+    // Requiere litros > 0
+    public void extraerSuper(int litros){
+        if (cantSuper > litros)
+            cantSuper -= litros;
+        else
+            cantSuper = 0;
+    }
+    // Requiere litros > 0
+    public void extraerPremium(int litros){
+        if (cantPremium > litros)
+            cantPremium -= litros;
+        else
+            cantPremium = 0;
+    }
+    // Consultas
+    public int obtenerMaximaCarga(){
+        return maximaCarga;
+    }
+    public int obtenerLitrosGasoil(){
+        return cantGasoil;
+    }
+    public int obtenerLitrosSuper(){
+        return cantSuper;
+    }
+    public int obtenerLitrosPremium(){
+        return cantPremium;
+    }
+    public boolean depositosLlenos(){
+        return cantGasoil == maximaCarga && cantSuper == maximaCarga && cantPremium == maximaCarga;
+    }
 }
