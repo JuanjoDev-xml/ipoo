70cccb075819b0de0044ea7f9e210a2ea2b7872c
Juan Jose Denk Guido <juanjosedenk05@gmail.com>
2025-08-28 17:05:04 -0300

    Ejercicio 3, inciso a

---
+++ tp2/ej3/Surtidor.java
@@ -0,0 +1,16 @@
+public class Surtidor{
+    // Atributos de clase
+    private static int maximaCarga = 500;
+    // Atributos de instancia
+    private int cantGasoil;
+    private int cantSuper;
+    private int cantPremium;
+    // Constructor
+    public Surtidor(){
+        cantGasoil = 20000;
+        cantSuper = 20000;
+        cantPremium = 20000;
+    }
+    // Comandos
+    
+}
