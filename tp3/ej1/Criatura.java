20eb390885e6bca5675ebb680a46a3493fae17a7
Juan Jose Denk Guido <juanjosedenk05@gmail.com>
2025-09-04 18:42:51 -0300

    Implementacion clase Criatura

---
+++ tp3/ej1/Criatura.java
@@ -0,0 +1,97 @@
+public class Criatura {
+    // Atributos de clase
+    private int maxEnergia = 100;
+    private int minEnergia = 0;
+    private int consumoEnergia = 10;
+    // Atributos de instancia
+    private int energia;
+    private Refugio refugio;
+    private boolean despierto;
+    private int caminatas;
+    // Constructor
+    public Criatura(Refugio r){
+        // Requiere r ligada
+        refugio = r;
+        energia = maxEnergia;
+        despierto = true;
+        caminatas = 0;
+    }
+    // Comandos
+    public boolean comer(){
+        boolean res = false;
+        if(despierto && refugio.obtenerAlimentos() > 0){
+            if (energia < maxEnergia) energia++;
+            refugio.consumirAlimento();
+            res = true;
+        }
+        return res;
+    }
+    public boolean beber(){
+        boolean res = false;
+        if(despierto && refugio.obtenerBebidas() > 0){
+            if (energia < maxEnergia) energia++;
+            refugio.consumirBebida();
+            res = true;
+        }
+        return res;
+    }
+    public boolean dormir(){
+        boolean res = false;
+        if(despierto && refugio.obtenerCamas() < 10){
+            refugio.ocuparCama();
+            despierto = false;
+            res = true;
+        }
+        return res;
+    }
+    public boolean despertar(){
+        boolean res = false;
+        if(!despierto){
+            refugio.desocuparCama();
+            despierto = true;
+            caminatas = 0;
+            res = true;
+        }
+        return res;
+    }
+    public boolean caminar(){
+        boolean res = true;
+        if (despierto && energia < 0) {
+            energia -= consumoEnergia;
+            if (caminatas < 4) caminatas++;
+            else{
+                res = false;
+                if(refugio.obtenerCamas() < 20){
+                    refugio.ocuparCama();
+                    dormir(); // aumenta caminatas ?????????????????????????????
+                }
+                else energia = minEnergia;
+            }
+        }
+        return res;
+    }
+    // Consultas
+    public int obtenerEnergia(){
+        return energia;
+    }
+    public int obtenerCaminatas(){
+        return caminatas;
+    }
+    public int obtenerHumor(){
+        int res = 1;
+        if (refugio.esHabitable() && energia > 40){
+            if (energia <= 70) res = 2;
+            else res = 3;
+        }
+        return res;
+    }
+    public boolean estaDormido(){
+        return !despierto; // está bien pensado ???????????????????
+    }
+    public boolean mayorEnergia(Criatura c){
+        return energia > c.obtenerEnergia(); // no hay que checkear c ligada??? creo que no
+    }
+    public String toSting(){
+        return energia + " " + refugio + " " + despierto + " " + caminatas;
+    }
+}
---
+++ tp3/ej1/Refugio.java
@@ -0,0 +1,126 @@
+public class Refugio{
+    // Atributos de clase
+    private static final int capacidadAlacena = 20;
+    private static final int cantidadCamas = 10;
+    // Atributos de instancia
+    private int alimentos, bebidas, camas;
+    // Constructor
+    public Refugio(int a, int b, int c){
+        if ((a + b) > capacidadAlacena){
+            alimentos = capacidadAlacena/2;
+            bebidas = capacidadAlacena/2;
+        }
+        else{
+            alimentos = a;
+            bebidas = b;
+        }
+        if(c > cantidadCamas){
+            camas = cantidadCamas;
+        }
+        else{
+            camas = c;
+        }
+    }
+    // Comandos
+    public void consumirAlimento(){
+        // Requiere controlar que haya alimento
+        alimentos--;
+    }
+    public void consumirBebida(){
+        // Requiere controlar que haya bebida
+        bebidas--;
+    }
+    public boolean ocuparCama(){
+        boolean res;
+        if (camas < cantidadCamas){
+            camas++;
+            res = true;
+        }
+        else{
+            res = false;
+        }
+        return res;
+    }
+    public boolean desocuparCama(){
+        boolean res;
+        if (camas != 0){
+            camas--;
+            res = true;
+        }
+        else{
+            res = false;
+        }
+        return res;
+    }
+    public boolean reponerAlimentos(int n){
+        boolean res;
+        if (n > 0 && n <= capacidadAlacena - alimentos - bebidas){
+            alimentos += n;
+            res = true;
+        }
+        else{
+            res = false;
+        }
+        return res;
+    }
+    public boolean reponerBebidas(int n){
+        boolean res;
+        if (n > 0 && n <= capacidadAlacena - alimentos - bebidas){
+            bebidas += n;
+            res = true;
+        }
+        else{
+            res = false;
+        }
+        return res;
+    }
+    // Consultas
+    public int obtenerAlimentos(){
+        return alimentos;
+    }
+    public int obtenerBebidas(){
+        return bebidas;
+    }
+    public int obtenerCamas(){
+        return camas;
+    }
+    public int obtenerCapacidadAlacena(){
+        return capacidadAlacena;
+    }
+    public boolean esHabitable(){
+        return alimentos > 0 || bebidas > 0 || camas < cantidadCamas;
+    }
+    public int disponibilidad(){
+        return cantidadCamas - camas;
+    }
+    public int diasSupervivencia(){
+        int res;
+        if (alimentos < bebidas){
+            res = alimentos;
+        }
+        else{
+            res = bebidas;
+        }
+        return res;
+    }
+    public boolean mayorAlimentos(Refugio r){
+        boolean res;
+        if (r != null && alimentos > r.obtenerAlimentos()){
+            res = true;
+        }
+        else{
+            res = false;
+        }
+        return res;
+    }
+    public boolean equals(Refugio r){
+        return r != null && r.obtenerAlimentos() == alimentos && r.obtenerBebidas() == bebidas
+                         && r.obtenerCamas() == camas;
+    }
+    public Refugio clone(){
+        return new Refugio(alimentos, bebidas, camas);
+    }
+    public String toString(){
+        return alimentos + " " + bebidas + " " + camas;
+    }
+}
