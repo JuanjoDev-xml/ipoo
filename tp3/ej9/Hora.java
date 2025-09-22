ab0eccb209a4e494a65610c5ae3730ecc77d9a13
Juan Jose Denk Guido <juanjosedenk05@gmail.com>
2025-09-21 21:50:34 -0300

    Modificacion de la clase Vehiculo, breve modificacion en Tester, compila perfectamente

---
+++ tp3/ej9/Hora.java
@@ -0,0 +1,50 @@
+public class Hora {
+    // Atributos de instancia
+    private int hor;
+    private int min;
+    // Constructor
+    public Hora(int h, int m){
+        hor = h;
+        min = m;
+    }
+    // Comandos
+    public void establecerHora(int c){
+        hor = c;
+    }
+    public void establecerMinutos(int c){
+        min = c;
+    }
+    public void copy(Hora h){
+        if (h != null){  // está bien que lo checkee? no dice requiere
+            hor = h.obtenerHora();
+            min = h.obtenerMinutos();
+        }
+    }
+    // Consultas
+    public int obtenerHora(){
+        return hor;
+    }
+    public int obtenerMinutos(){
+        return min;
+    }
+    public boolean equals(Hora c){
+        // Requiere c ligado
+        return hor == c.obtenerHora() && min == c.obtenerMinutos();
+    }
+    public int diferenciaMinutos(Hora c){
+        int res = 0; // no aclara qué retornar si c no está ligado
+        if (c != null){ // está bien que lo checkee?????????????
+            int minLocal = 60*hor + min;
+            int minC = 60*(c.obtenerHora()) + c.obtenerMinutos();
+            res = minLocal - minC;
+            if (res < 0) res = res*(-1); // para tener el valor absoluto
+        }
+        return res;
+    }
+    private boolean horaAnterior(Hora h){
+        return hor < h.obtenerHora() || (hor == h.obtenerHora() && min < h.obtenerMinutos()); 
+    }
+    public boolean anterior(Hora c){
+        return c != null && horaAnterior(c); // está bien que lo checkee????????????? 
+    }
+}
---
+++ tp3/ej9/Vehiculo.java
@@ -0,0 +1,55 @@
+public class Vehiculo {
+    // Atributos de instancia
+    private Hora ingreso;
+    private int aCobrar;
+    private int numero;
+    private String patente;
+    // Constructor
+    public Vehiculo(Hora i, int n, String p){
+        aCobrar = 0;
+        ingreso = establecerIngreso(i);
+        numero = n;
+        patente = p;
+    }
+    private Hora establecerIngreso(Hora i){ // está bien? no se aclara qué hacer si la hora no es válida
+        if (i.obtenerHora() < 7 || i.obtenerHora() > 22) i.establecerHora(7);
+        if (i.obtenerMinutos() < 0 || i.obtenerMinutos() > 59) i.establecerMinutos(0);
+        return i;
+    }
+    // Comandos
+    public void egresaVehiculo(Hora c, Tarifa t){
+        int diferencia = ingreso.diferenciaMinutos(c);
+        if (diferencia <= 15) aCobrar = t.obtenerT15();
+        if (diferencia > 15 && diferencia <= 30) aCobrar = t.obtenerT30();
+        if (diferencia > 30 && diferencia <= 60) aCobrar = t.obtenerT60();
+        if (diferencia > 60) aCobrar = t.obtenerTFija();
+    }
+    public void copy(Vehiculo v){
+        // Requiere v ligado y vehiculo egresado
+        ingreso.copy(v.obtenerIngreso());
+        numero = v.obtenerNumero();
+        patente = v.obtenerPatente();
+    }
+    // Consultas
+    public Hora obtenerIngreso(){
+        return ingreso;
+    }
+    public int obtenerNumero(){
+        return numero;
+    }
+    public String obtenerPatente(){
+        return patente;
+    }
+    public int obtenerCobrar(){
+        return aCobrar;
+    }
+    public boolean equals(Vehiculo v){
+        // Requiere v ligado, se implementa en profundidad
+        return ingreso.equals(v.obtenerIngreso()) && numero == v.obtenerNumero()
+                && patente == v.obtenerPatente();
+    }
+    public boolean anterior(Vehiculo v){
+        // Requiere v ligado
+        return ingreso.anterior(v.obtenerIngreso());
+    }
+}
