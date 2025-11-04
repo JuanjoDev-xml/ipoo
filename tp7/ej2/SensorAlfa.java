27c6d7e0b12b47f854270cf84004f2382e98c88a
Juan Jose Denk Guido <juanjosedenk05@gmail.com>
2025-11-04 10:00:49 -0300

    Ejercicio 2

---
+++ tp7/ej2/SensorAlfa.java
@@ -0,0 +1,55 @@
+public class SensorAlfa extends Sensor{
+    // Atributos de instancia
+    protected float p3;
+    protected float p4;
+    // Constructor
+    public SensorAlfa(float pU, float pD, float pT, float pC){
+        super(pU, pD);
+        p3 = pT;
+        p4 = pC;
+    }
+    // Comandos
+    public void establecerP3(float p){
+        p3 = p;
+    }
+    public void establecerP4(float p){
+        p4 = p;
+    }
+    public void copy(SensorAlfa s){
+        if (s != null){
+            super.copy(s);
+            p3 = s.obtenerP3();
+            p4 = s.obtenerP4();
+        }
+    }
+    // Consultas
+    public float obtenerP3(){
+        return p3;
+    }
+    public float obtenerP4(){
+        return p4;
+    }
+    public boolean riesgo(){
+        return super.riesgo() || p4 > p3;
+    }
+    public boolean emergencia(){
+        return p4 < max;
+    }
+    public SensorAlfa clone(){
+        return new SensorAlfa(p1, p2, p3, p4);
+    }
+    public boolean equals(Sensor e){
+        boolean ig;
+        if (this == e)
+            ig = true;
+        else if (e == null)
+            ig = false;
+        else if(getClass() != e.getClass())
+            ig = false;
+        else {
+            SensorAlfa r = (SensorAlfa) e;
+            ig = super.equals(e) && p3 == r.obtenerP3() && p4 == r.obtenerP4();
+        }
+        return ig;
+    }
+}
