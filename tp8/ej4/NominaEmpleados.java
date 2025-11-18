8db63552e409773eae44828deb5ce562909202b2
Juan Jose Denk Guido <juanjosedenk05@gmail.com>
2025-11-18 19:38:16 -0300

    Implementación clase NominaEmpleados completa

---
+++ tp8/ej4/NominaEmpleados.java
@@ -0,0 +1,24 @@
+public class NominaEmpleados extends Coleccion{
+    // Constructor
+    public NominaEmpleados(int n){
+        super(n);
+    }
+    // Consultas
+    public int contarSupHoras(int h){
+        int cantidad = 0;
+        for (int i = 0; i < cant; i++){
+            Empleado e = (Empleado) col[i];
+            if (e.obtenerCantHoras() > h)
+                cantidad++;
+        }
+        return cantidad;
+    }
+    public float sumarSueldos(){
+        float res = 0;
+        for (int i = 0; i < cant; i++){
+            Empleado e = (Empleado) col[i];
+            res += e.obtenerSueldo();
+        }
+        return res;
+    }
+}
