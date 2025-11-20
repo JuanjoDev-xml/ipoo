b0e6d34c1be058823bc82165dfce0318747ff91a
Juan Jose Denk Guido <juanjosedenk05@gmail.com>
2025-11-19 22:28:10 -0300

    Planteo inciso c

---
+++ tp8/ej7/Tribu.java
@@ -0,0 +1,12 @@
+public class Tribu extends Conjunto{
+    // Constructor
+    public Tribu(int max){
+        // Requiere max > 0
+        super(max);
+    }
+    // Consultas
+    public boolean hayTresDecrecientes(){
+        boolean res = true;
+        return res;
+    }
+}
